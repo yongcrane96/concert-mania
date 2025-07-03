1f8b912703e87fa00d50a660213239d4aa1320a6
yongcrane96 <dydgkr3363@naver.com>
2025-07-03 17:54:24 +0900

    Add files via upload

---
+++ src/main/java/com/example/concertMania/ConcertManiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertManiaApplication.class, args);
+	}
+
+}
